b1fefc2a74bdefad61960f4c4bb17c42baeb74bd
Andres Felipe Gomez Barbosa <afgomezb20@gmail.com>
2018-10-03 10:41:55 -0500

    the warnings checktyles are modifiqued

--- src/main/java/com/example/notasfaciles/NotasFacilesApplication.java
+++ src/main/java/com/example/notasfaciles/NotasFacilesApplication.java
@@ -1,13 +1,26 @@
+/**
+ * This is a test app
+ *
+ * @autor Andrés Felipe Gómez Barbosa
+ */
 package com.example.notasfaciles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * Main class NotasFaciles app
+ */
 @SpringBootApplication
 @EnableJpaAuditing
 public class NotasFacilesApplication {
 
+	/**
+	 * Main Metod app NotasFaciles
+	 *
+	 * @param args
+	 */
 	public static void main(final String[] args) {
 		SpringApplication.run(NotasFacilesApplication.class, args);
 	}
