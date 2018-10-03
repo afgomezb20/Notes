72340854e7a5d80ceea6413a45a2d3a8d6d1a5f6
Andres Felipe Gomez Barbosa <afgomezb20@gmail.com>
2018-10-03 10:28:48 -0500

    the warnings checktyles are modifiqued

--- src/main/java/com/example/notasfaciles/NotasFacilesApplication.java
+++ src/main/java/com/example/notasfaciles/NotasFacilesApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class NotasFacilesApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(NotasFacilesApplication.class, args);
 	}
 }
